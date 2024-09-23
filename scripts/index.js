dc83738940585a825054f7ef959e417166a850b4
Anna Ro <roanna.97line@gmail.com>
2024-09-23 14:24:56 +0300

    Базовая фенкциональность

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
+const placesWrap = document.querySelector(".places__list");
+
+function createCard(cardData, deleteCard) {
+    const card = cardTemplate.cloneNode(true);
+    
+    const cardTitle = card.querySelector(".card__title");
+    cardTitle.textContent = cardData.name;
+    
+    const cardImage = card.querySelector(".card__image");
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    const deleteButton = card.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", deleteCard);
+
+    return card;
+}
+
+function deleteCard(evt) {
+    evt.target.closest(".card").remove();
+}
+
+initialCards.forEach((cardData) => {
+    placesWrap.append(createCard(cardData, deleteCard));
+})
